79973ce856a6ef643ac550c350681ec1088e6638
Andres Garcia Fiorini <andres.fiorini@gmail.com>
2017-07-28 15:33:11 -0300

    .

--- generate-sandbox.sh
+++ generate-sandbox.sh
@@ -4,7 +4,7 @@ set -e -x
 
 SCRIPTPATH=$( cd $(dirname $0) ; pwd -P )
 
-SECRETS=$SCRIPTPATH/cf-secrets-staging.yml
+SECRETS=$SCRIPTPATH/cf-secrets.sandbox.*.yml
 MANIFEST=$SCRIPTPATH/manifest-staging.yml
 
 if [ "${#@}" -gt 0 ]
